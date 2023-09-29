0453a5cef204f4583ce22c2377bae103d9adf51a
Nick Cardin <nick@cardin.email>
2023-09-29 14:00:53 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use tokio_stream::StreamExt;
+
+use fluvio::metadata::topic::TopicSpec;
+use fluvio::{Fluvio, RecordKey};
+
+const TOPIC_NAME: &str = "hello-rust";
+const PARTITION_NUM: u32 = 0;
+const PARTITIONS: u32 = 1;
+const REPLICAS: u32 = 1;
+
+#[tokio::main]
+async fn main() {
+   // Connect to Fluvio cluster
+   let fluvio = Fluvio::connect().await.unwrap();
+
+   // Create a topic
+   let admin = fluvio.admin().await;
+   let topic_spec = TopicSpec::new_computed(PARTITIONS, REPLICAS, None);
+   let _topic_create = admin
+       .create(TOPIC_NAME.to_string(), false, topic_spec)
+       .await;
+
+   // Create a record
+   let record = format!("Hello World!");
+
+   // Produce to a topic
+   let producer = fluvio::producer(TOPIC_NAME).await.unwrap();
+   producer.send(RecordKey::NULL, record).await.unwrap();
+   // Fluvio batches outgoing records by default, so flush producer to ensure all records are sent
+   producer.flush().await.unwrap();
+
+   // Consume last record from topic
+   let consumer = fluvio::consumer(TOPIC_NAME, PARTITION_NUM).await.unwrap();
+   let mut stream = consumer.stream(fluvio::Offset::from_end(1)).await.unwrap();
+   if let Some(Ok(record)) = stream.next().await {
+       let string = String::from_utf8_lossy(record.value());
+       println!("{}", string);
+   }
+}
